7cac58b0d26de430a9141e670cc4e2d5faecc94d
Kenny-Wagner <47788231+Kenny-Wagner@users.noreply.github.com>
2024-07-20 17:30:35 +0000

    Improved look of Header

--- frontend/src/components/HeaderMenu.jsx
+++ frontend/src/components/HeaderMenu.jsx
@@ -1,44 +1,53 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { Menu, Group, Center, Button, Divider, Container } from '@mantine/core';
+import { Menu, Group, Center, Button, Container, Box, Flex } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown } from '@tabler/icons-react';
 import classes from './HeaderMenu.module.css';
 
-const HeaderMenu = ({user, onLogout}) => {
+const HeaderMenu = ({ user, onLogout }) => {
   const links = [
-    { link: '/', label: 'Collections' },
+    {
+      link: '#1',
+      label: 'Collections',
+      links: [
+        { link: '/', label: 'All' },
+        { link: '/3', label: 'Collection 2' },
+        { link: '/4', label: 'Collection 3' },
+      ],
+    },
     { link: '/about', label: 'About' },
     { link: '/contact', label: 'Contact' },
-    user?.isAdmin ? { link: '/manage-art', label: 'Manage Art' } : null
+    user?.isAdmin ? { link: '/manage-art', label: 'Manage Art' } : null,
   ].filter(Boolean);
 
   const [opened, { toggle }] = useDisclosure(false);
-  
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleLoginLogout =() => {
+  const handleLoginLogout = () => {
     if (!user) {
-      navigate('/login')
+      navigate('/login');
     } else {
-      onLogout()
+      onLogout();
     }
-  }
+  };
+
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>
+      <Menu.Item key={item.link}>    
         <Link to={item.link} className={classes.link}>
           {item.label}
-        </Link>
+         </Link>
       </Menu.Item>
     ));
+
 
     if (menuItems) {
       return (
         <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
           <Menu.Target>
-            <a href={link.link} className={classes.link} onClick={(event) => event.preventDefault()}>
+            <a href={link.link} className={classes.link} onClick={(event) => navigate(link.link)}>
               <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
+                <span>{link.label}</span>
                 <IconChevronDown size="0.9rem" stroke={1.5} />
               </Center>
             </a>
@@ -58,21 +67,22 @@ const HeaderMenu = ({user, onLogout}) => {
   return (
     <header className={classes.header}>
       <Container size="lg">
-        <div className={classes.inner}>
-          <div className={classes.logo}>Rose Portfolio</div>
-          <Group spacing={5} className={classes.links}>
+        <Flex className={classes.inner} justify="space-between" align="center">
+          <Box className={classes.logo}>Tibial Rose</Box>
+          <Group className={classes.links}>
             {items}
           </Group>
-          <Divider my="sm" />
-          <Group pb="xl" px="md" mt="md">
+          <Group className={classes.authButtons}>
             <Button variant="default" onClick={handleLoginLogout}>
-              {user ? 'Log out': 'Log in'}
+              {user ? 'Log out' : 'Log in'}
             </Button>
-            {!user && <Button onClick ={() => navigate('/register')}> 
-              Register
-            </Button>} 
+            {!user && (
+              <Button className={classes.authButton} onClick={() => navigate('/register')}>
+                Register
+              </Button>
+            )}
           </Group>
-        </div>
+        </Flex>
       </Container>
     </header>
   );

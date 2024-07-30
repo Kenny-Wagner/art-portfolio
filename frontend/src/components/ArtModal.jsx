2444aeb07ced5acacffc1cc2011d147f7a0ae6a7
Kenny Wagner <kennyacdc2@gmail.com>
2024-07-29 23:00:55 -0400

    Improved responsiveness of art modal

--- frontend/src/components/ArtModal.jsx
+++ frontend/src/components/ArtModal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Image, Flex, Paper, Title, Text, Stack } from '@mantine/core';
+import { Modal, Image, Flex, Paper, Title, Text, Stack, Container, Center } from '@mantine/core';
 import styles from './ArtModal.module.css';
 
 const ArtModal = ({ artpiece, opened, close }) => {
@@ -15,29 +15,30 @@ const ArtModal = ({ artpiece, opened, close }) => {
           backgroundOpacity: 0.55,
           blur: 3,
         }}
-        size="auto" // Auto size the modal based on content
+        size="auto"
         transitionProps={{
-          transition: 'pop', // Add transition type
-          duration: 400, // Set transition duration (in ms)
+          transition: 'pop',
+          duration: 400,
           timingFunction: "ease"
-        }} 
-        styles={{
-          modal: styles.modal,
         }}
       >
-        <Flex direction="row" wrap="nowrap" gap='md' className={styles.flexContainer}>
-          <Image radius='sm'src={artpiece.imageUrl} className={styles.image} />
-          <Flex direction="column" className={styles.flexColumn}>
+        <Flex direction={{ base: 'column', sm: 'row' }} gap='md'>
+          <Center>
+            <Container size="xs" className={styles.container}>
+            <Image radius='sm' src={artpiece.imageUrl} className={styles.image} />
+          </Container>
+          </Center>
+          <Flex direction="column">
             <Paper shadow="xs" p="xl" mb='md' className={styles.paperTitle}>
               <Title align='center' mb='xl'>{artpiece.title}</Title>
               <Stack spacing="xs" align="flex-start">
-                <Text>2024</Text>
-                <Text>11x17</Text>
-                <Text>Acrylic on canvas</Text>
+                <Text>{artpiece.year}</Text>
+                <Text>{artpiece.size}</Text>
+                <Text>{artpiece.medium}</Text>
               </Stack>
             </Paper>
             <Paper shadow="xs" p="xl" className={styles.paperDescription}>
-              <Text>word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word word </Text>
+              <Text>{artpiece.description}</Text>
             </Paper>
           </Flex>
         </Flex>

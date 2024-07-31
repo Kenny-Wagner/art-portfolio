58b76ae37735eab1c5d71263da333dace5657fe1
Kenny <kennyacdc2@gmail.com>
2024-07-31 00:42:44 +0000

    Fix error where non-api routes errored out, fixed title

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const artRoutes = require('./routes/art');
@@ -9,10 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use(express.static('dist'))
 app.use(express.static('public'))
 app.use('/api/auth', authRoutes);
 app.use('/api/art', artRoutes);
 app.use('/api/s3', s3Routes)
+
+// Catch-all route to serve React's index.html for all non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 module.exports = app

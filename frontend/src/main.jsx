355c7384d3fb377a923b6166d5516df8b555cc04
Kenny Wagner <kennyacdc2@gmail.com>
2024-11-01 22:13:13 -0400

    Fix issue where header icons did not span full header after router changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import "@mantine/core/styles.css"; // Do not remove
 import '@mantine/carousel/styles.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
